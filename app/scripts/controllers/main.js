67eadf69ce7e9d85f2d75bb37e989f03375aa9fd
Mashallah Behbehani <mashallah.behbehani@warrendouglas.com>
2013-08-01 13:00:40 -0500

    Layout tweaks

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,7 +5,7 @@ var nectarineApp = angular.module('nectarineApp')
 
   // constants
   var margin = 20,
-    width = 960,
+    width = 500,
     height = 500 - .5 - margin,
     color = d3.interpolateRgb("#f77", "#77f");
 
